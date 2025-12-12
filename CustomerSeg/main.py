f8b803df2c03cfa14b6a1fa87163554ea6731a85
pawan <pawansinghshiva2030@gmail.com>
2025-12-12 16:17:35 +0530

    deleted unrelated files and data

--- CustomerSeg/main.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-def main():
-    print("Starting AQI Prediction App...")
-    os.system(f'"{sys.executable}" -m streamlit run app/streamlit_app.py')
-
-if __name__ == "__main__":
-    main()
